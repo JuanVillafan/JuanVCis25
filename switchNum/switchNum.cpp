663bbe975f170cfdf91858428eaf678cbe0bb775
Juan <Juan@DESKTOP-LHDFCR4>
2024-09-03 22:48:45 -0700

    Add project files.

---
+++ switchNum/switchNum.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 2;
+    int num2 = 1;
+    cout << "Original Value : " << endl;
+    cout << "num1 = " << num1 << endl << "num2 = " << num2 << endl;
+    int placehold;
+    placehold = num1;
+    num1 = num2;
+    num2 = num1;
+    cout << "Switched Values : " << endl;
+    cout << "num1 = " << num1 << endl << "num2 = " << num2 << endl;
+
+}
